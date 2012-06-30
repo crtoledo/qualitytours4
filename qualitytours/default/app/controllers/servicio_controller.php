f376d8172ba260c2fdb4376fc07f34dc4331c33e
kerosene90 <kerosene90@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-06-30 01:05:18 +0000

    Ingresar servicio 97% -> Validado el form de ingreso (menos el textarea que no lo pude validar :/ ) y ya ingresa las publicaciones a la bd

--- qualitytours/default/app/controllers/servicio_controller.php
+++ qualitytours/default/app/controllers/servicio_controller.php
@@ -15,7 +15,7 @@ class ServicioController extends AppController
     
     public function ingresar()
     { 
-          //verifica si se encuentra logueado
+        //verifica si se encuentra logueado
 	if(!Auth::is_valid())
         {
                 Flash::info('Debe iniciar sesión');
@@ -29,11 +29,34 @@ class ServicioController extends AppController
                 Flash::info('No posee los privilegios necesarios');
                 Router::redirect("/");
             }
+            //POSEE LOS PRIVILEGIOS
             else
             {
-                $cliente = new Cliente();
-                $cliente->find(Auth::get('id'));
-                $this->nombre_cliente = $cliente->nombre_cli;
+                //SI HAY DATOS PARA INGRESAR SALVARLOS A LA BD ENTRA AL IF
+                if (Input::hasPost('servicio'))
+                {
+                    $servicio = new Servicio(Input::post('servicio'));
+            
+                    if($servicio->save())
+                    {
+                    Flash::success('Servicio publicado satisfactoriamente');
+                    Input::delete();
+                    Router::redirect('/');
+                    }
+                    else
+                    {
+                        Flash::error('Error al publicar el servicio');
+                    }
+                
+                }
+                //DE LO CONTRARIO OBTENEMOS EL NOMBRE DEL CLIENTE PARA EL FORM DE INGRESO
+                else
+                {
+                    $cliente = new Cliente();
+                    $cliente->find(Auth::get('id'));
+                    $this->nombre_cliente = $cliente->nombre_cli;   
+                }
+                
                 
                 //Ingreso del servicio a la BD
             } 
