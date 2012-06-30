090f831460362c97e90d3c06588b0feb425bc2c6
kerosene90 <kerosene90@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-06-30 04:53:09 +0000

    primer commit de archivo

--- trunk/qualitytours/default/app/controllers/contenido_controller.php
+++ trunk/qualitytours/default/app/controllers/contenido_controller.php
@@ -47,20 +47,29 @@ class ContenidoController extends AppController
         
     
     public function imagen() {
-        View::select('index');  //para mostrar siempre la vista con los formularios
-        if (Input::hasPost('oculto')) {  //para saber si se envió el form
+       // View::select('index');  //para mostrar siempre la vista con los formularios
+        if (Input::hasPost('oculto')) 
+        {  //para saber si se envió el form
  
             //llamamos a la libreria y le pasamos el nombre del campo file del formulario
             //el segundo parametro de Upload::factory indica que estamos subiendo una imagen
             //por defecto la libreria Upload trabaja con archivos...
-            $archivo = Upload::factory('archivo', 'image'); 
+            $archivo = Upload::factory('imagen', 'image'); 
             $archivo->setExtensions(array('jpg', 'png', 'gif'));//le asignamos las extensiones a permitir
-            if ($archivo->isUploaded()) {
-                if ($archivo->save()) {
+            if ($archivo->isUploaded()) 
+            {
+                if ($archivo->save())
+                {
                     Flash::valid('Imagen subida correctamente...!!!');
                 }
-            }else{
-                    Flash::warning('No se ha Podido Subir la imagen...!!!');
+                else
+                {
+                    Flash::warning('No se ha Podido guardar la imagen...!!!');
+                }
+            }
+            else
+            {
+                Flash::warning('No se ha Podido transmitir la imagen...!!!');
             }
         }
     }
