35778f411e007c3abe03b3a6178f52878a194de2
gbriones1234@gmail.com <gbriones1234@gmail.com@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-06-30 22:28:47 +0000

    ingreso de adminitrador para subir imagenes asociado a publicaciones existentes.

--- trunk/qualitytours/default/app/controllers/contenido_controller.php
+++ trunk/qualitytours/default/app/controllers/contenido_controller.php
@@ -23,7 +23,7 @@ class ContenidoController extends AppController
             //verifica si el rol pertenece como corresponde
             else
             {
-                 if(Auth::get('rol_usu') != 'administrador')
+                 if(Auth::get('rol_usu') != 'administrador' && Auth::get('rol_usu') != 'cliente')
                 {
                     Flash::info('No posee los privilegios necesarios');
 		    Router::redirect("/");
@@ -40,15 +40,16 @@ class ContenidoController extends AppController
         
          public function ingresar($id) 
       {
-  
+             //CAPTURA EL ID DELA PUBLICACION PARA ENVIARLA A LA VISTA
+             $this->id_pub = $id;
          
       }
       
         
-    
-    public function imagen() {
-       // View::select('index');  //para mostrar siempre la vista con los formularios
-        if (Input::hasPost('oculto')) 
+    //FUNCION PARA SUBIR CONTENIDO DEL ADMINISTRADOR
+    public function admincont() {
+       
+        if (Input::hasPost('contenido')) 
         {  //para saber si se envió el form
  
             //llamamos a la libreria y le pasamos el nombre del campo file del formulario
@@ -56,24 +57,35 @@ class ContenidoController extends AppController
             //por defecto la libreria Upload trabaja con archivos...
             $archivo = Upload::factory('imagen', 'image'); 
             $archivo->setExtensions(array('jpg', 'png', 'gif'));//le asignamos las extensiones a permitir
-           
+            
+            $contenido = new Contenido(Input::post('contenido'));
+            //OBTIENE LOS PARAMETROS DEL FORMULARIO
+            $id_pub = $contenido->id_pub;
+            $fecha = $contenido->fecha_con;
+            $nombre = $contenido->nombre_con;
+            $tipo = $contenido->tipo_con;
+      
             if ($archivo->isUploaded()) 
-            {
-                if ($archivo->saveRandom())
-                {
+            {     //CAPTURA LA RUTA TRANSFOMADA EN MD5 PARA GUARDARLA EN LA BD
+                if($ruta = $archivo->saveRandom())
+                {   
+
                     Flash::valid('Imagen subida correctamente...!!!');
-                    Flash::info();
-                    Router::redirect('/');
+                    if($contenido->sql("insert into Contenido(id_pub, ruta_con, fecha_con, nombre_con, tipo_con,estado_con)values(".$id_pub.",'".$ruta."','".$fecha."','".$nombre."','".$tipo."','t')"))
+                    {   
+                     Flash::success('Contenido Ingresado Exitosamente.');
+                     Router::redirect("/");
+                    }       
                 }
                 else
                 {
                     Flash::warning('No se ha Podido guardar la imagen...!!!');
                 }
             }
-            else
-            {
-                Flash::warning('No se ha Podido transmitir la imagen...!!!');
-            }
+                else
+                {
+                    Flash::warning('No se ha Podido transmitir la imagen...!!!');
+                }
         }
     }
 
