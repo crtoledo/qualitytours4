6a4e06cb4084db7ac60128573dfb4c92b1673c88
kerosene90 <kerosene90@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-07-02 05:56:01 +0000

    UBICACION -> ya ingresa la ubicacion a la BD

--- trunk/qualitytours/default/app/controllers/ubicacion_controller.php
+++ trunk/qualitytours/default/app/controllers/ubicacion_controller.php
@@ -19,6 +19,21 @@ class UbicacionController extends AppController
                 $cliente->find(Auth::get('id'));
                 $this->nombre_cliente = $cliente->nombre_cli;
                 $this->id_usu = Auth::get('id');
+                if (Input::hasPost('ubicacion'))
+                {
+                    $ubicacion = new Ubicacion(Input::post('ubicacion'));
+                    if ($ubicacion->save())
+                    {
+                        Flash::success('Su ubicación fue registrada');
+                        Input::delete();
+                        Router::redirect("/");
+                    }
+                    else
+                    {
+                        Flash::info('Error en el ingreso de su ubicacion');
+                        Router::redirect('/');
+                    }
+                }
                 
             }
             else //NO ES CLIENTE
