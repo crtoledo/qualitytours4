56d97bd4a723df17d832f628e4e1e29f962561a7
gbriones1234@gmail.com <gbriones1234@gmail.com@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-10-11 17:35:15 +0000

    publicacion listado correctamente.

--- qualitytours/default/app/controllers/index_controller.php
+++ qualitytours/default/app/controllers/index_controller.php
@@ -1,4 +1,6 @@
 <?php
+Load::model('publicacion');
+Load::model('contenido');
 /**
  * Controller por defecto si no se usa el routes
  * 
@@ -7,16 +9,42 @@ class IndexController extends AppController
 {
 	public function index()
 	{
+            //Creamos objetos que necesitaremos
+            $publicacion = new Publicacion();
+           
             //Capturan los valores de la publicación
-             $arr= Load::model('publicacion')->find("conditions: estado_pub='t'","order: id DESC"); 
-             
+             $arr= $publicacion->find("conditions: estado_pub='t'","order: id DESC");
+       
+       
+              //validar si existen publicaciones
+           if ($publicacion->count() == 0)
+            {
+               $this->cont = 0;
+              
+            }
+            else
+            {
+                $this->cont = 1;
+            }
              //recorre el arreglo de publicacion
              foreach ($arr as $publicacion ) 
              {
                 $this->titulo = $publicacion->titulo_pub; 
                 $this->detalle = $publicacion->detalle_pub;
+                   //crear objeto
+                   $contenido = new Contenido();
                    //BUCAR IMAGENES ASOCIADAS AL ID DE LA PUBLICACION 
-                      $imagen= Load::model('contenido')->find("conditions: id_pub=".$publicacion->id);
+                  //validar si existen contenido
+                   if ($contenido->count() == 0)
+                    {
+                        $this->contenido = 0;
+              
+                    }
+                    else
+                    {
+                        $this->contenido = 1;
+                     }
+                      $imagen= $contenido->find("conditions: id_pub=".$publicacion->id);
                        foreach($imagen as $buscar)
                        {
                            $this->ruta = $buscar->ruta_con;
