641531cf805c4af7257d04cd0cebfe349399112f
gbriones1234@gmail.com <gbriones1234@gmail.com@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-10-11 15:46:15 +0000

    limpieza de modelos a controlador en index.phtml  a index_controller quedo 100% limpio

--- qualitytours/default/app/controllers/index_controller.php
+++ qualitytours/default/app/controllers/index_controller.php
@@ -7,6 +7,26 @@ class IndexController extends AppController
 {
 	public function index()
 	{
+            //Capturan los valores de la publicación
+             $arr= Load::model('publicacion')->find("conditions: estado_pub='t'","order: id DESC"); 
+             
+             //recorre el arreglo de publicacion
+             foreach ($arr as $publicacion ) 
+             {
+                $this->titulo = $publicacion->titulo_pub; 
+                $this->detalle = $publicacion->detalle_pub;
+                   //BUCAR IMAGENES ASOCIADAS AL ID DE LA PUBLICACION 
+                      $imagen= Load::model('contenido')->find("conditions: id_pub=".$publicacion->id);
+                       foreach($imagen as $buscar)
+                       {
+                           $this->ruta = $buscar->ruta_con;
+                           
+                           
+                       }
+                 
+                $this->fecha = $publicacion->fecha_pub;
+                $this->id = $publicacion->id;
+             }
 		
 	}
 }
