2192f8d2851dc3f7918cccd56cdcc7814b657439
kerosene90 <kerosene90@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-10-11 19:06:20 +0000

    Categoria funcionando con listbox, ya ingresa categorias a la BD. FALTARIA SOLO VALIDACION AL NO SELECCIONAR NINGUN CENTRO

--- qualitytours/default/public/ajax/cliente_busqueda.php
+++ qualitytours/default/public/ajax/cliente_busqueda.php
@@ -1,17 +1,17 @@
 <?php
-//Busca el nombre de los cliente que contengan el string dado
+//Busca el nombre y id de los cliente que contengan el string dado
 //el string se pasa por la url GET
 //?string=xxx
 require_once 'connection.php';
 $Db = new db();
 $conn = $Db->open();
 
-$resu = pg_query($conn,"select nombre_cli from cliente where nombre_cli ilike '%".$_REQUEST['string']."%'");
-$res = pg_fetch_all_columns($resu);
+$resu = pg_query($conn,"select nombre_cli,id_usu from cliente where nombre_cli ilike '%".$_REQUEST['string']."%'");
+$res = pg_fetch_all($resu);
 
 if (is_array($res) && isset($res[0]))
-{       
-    echo json_encode($res);
+{    
+    echo json_encode($res); 
 }
 else
     return null;
