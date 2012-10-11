cbb74c30d4c66f5a850527e23ad4283675a43d7b
gbriones1234@gmail.com <gbriones1234@gmail.com@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-10-11 19:10:11 +0000

    detalles

--- qualitytours/default/app/controllers/index_controller.php
+++ qualitytours/default/app/controllers/index_controller.php
@@ -11,50 +11,43 @@ class IndexController extends AppController
 	{
             //Creamos objetos que necesitaremos
             $publicacion = new Publicacion();
-           
-            //Capturan los valores de la publicación
-             $arr= $publicacion->find("conditions: estado_pub='t'","order: id DESC");
-       
-       
-              //validar si existen publicaciones
-           if ($publicacion->count() == 0)
+            //validar si existen publicaciones
+            if($publicacion->count("conditions: estado_pub='t'") == 0)
             {
-               $this->cont = 0;
-              
+              $this->cont = 0;
             }
             else
             {
-                $this->cont = 1;
+              $this->cont = 1;
             }
+            //Capturan los valores de la publicación
+             $arr= $publicacion->find("conditions: estado_pub='t'","order: id DESC");
              //recorre el arreglo de publicacion
              foreach ($arr as $publicacion ) 
              {
                 $this->titulo = $publicacion->titulo_pub; 
                 $this->detalle = $publicacion->detalle_pub;
-                   //crear objeto
-                   $contenido = new Contenido();
-                   //BUCAR IMAGENES ASOCIADAS AL ID DE LA PUBLICACION 
-                  //validar si existen contenido
-                   if ($contenido->count() == 0)
-                    {
-                        $this->contenido = 0;
-              
-                    }
-                    else
-                    {
-                        $this->contenido = 1;
-                     }
-                      $imagen= $contenido->find("conditions: id_pub=".$publicacion->id);
-                       foreach($imagen as $buscar)
-                       {
-                           $this->ruta = $buscar->ruta_con;
-                           
-                           
-                       }
-                 
+                //crear objeto
+                $contenido = new Contenido();
+                //BUCAR IMAGENES ASOCIADAS AL ID DE LA PUBLICACION 
+                //validar si existen contenido
+                if ($contenido->count("conditions: estado_con='t'") == 0 )
+                {
+                    $this->contenido = 0;
+                }
+                else
+                {
+                    $this->contenido = 1;
+                }
+                $imagen= $contenido->find("conditions: id_pub=".$publicacion->id."and  estado_con='t'");
+                foreach($imagen as $buscar)
+                {
+                    $this->ruta = $buscar->ruta_con;
+                }
                 $this->fecha = $publicacion->fecha_pub;
                 $this->id = $publicacion->id;
              }
 		
 	}
 }
+?>
