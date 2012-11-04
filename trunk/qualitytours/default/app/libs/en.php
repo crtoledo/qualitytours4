452baa327d2f3c44d281e7939146c8ea626bc168
gbriones1234@gmail.com <gbriones1234@gmail.com@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-11-04 03:26:42 +0000

    ingreso de usuario mas el parametro lenguaje como false  y idioma correspondiente

--- trunk/qualitytours/default/app/libs/en.php
+++ trunk/qualitytours/default/app/libs/en.php
@@ -10,5 +10,14 @@ $con_ingresar_string="en";
 $pub_editar_string="en";
 
 //variables para vista /usuario/ingresar
+$titulo_ingresar_string = "Register";
+$ingresar_usuario_string= "Create Account";
+$nombre_ingresar_string ="Name";
+$apellido_ingresar_string = "Last name";
+$val_username_string = "Please enter username";
+$val_password_string = "Please enter password";
+$val_nombre_string = "Please enter your name";
+$val_apellido_string = "Please enter your last name";
+$val_rut_string = "Please enter your ID";
+$val_email_string = "Please enter your email";
 
-?>
