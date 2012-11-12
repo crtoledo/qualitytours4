1ab55fefe4b2d6ca29b22fe09533ea01a428b017
kerosene90 <kerosene90@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-11-12 17:19:55 +0000

    Ingreso por mapa habilitado y pequeña revisión de variables de idioma

--- trunk/qualitytours/default/app/controllers/ubicacion_controller.php
+++ trunk/qualitytours/default/app/controllers/ubicacion_controller.php
@@ -62,4 +62,58 @@ class UbicacionController extends AppController
             Router::redirect("/");
         }
     }
+    
+    public function ingresar_mapa()
+    {
+        if (Auth::is_valid())
+        {
+            if(Auth::get('rol_usu') == 'cliente')
+            {
+                $cliente = new Cliente();
+                $cliente->find(Auth::get('id'));
+                $this->nombre_cliente = $cliente->nombre_cli;
+                $this->id_usu = Auth::get('id');
+                
+                $v_ubi = new Ubicacion();
+                if($v_ubi->count("conditions: id_usu=".Auth::get('id')) == 0) //Verificamos que no exista ya la ubicacion
+                {
+
+                    if (Input::hasPost('ubicacion'))
+                    {
+                        $ubicacion = new Ubicacion(Input::post('ubicacion'));
+                        if ($ubicacion->save())
+                        {
+                            //Actualizamos el id_ubi en cliente
+                            $cliente->sql("update Cliente set id_ubi=(select id from Ubicacion where id_usu=".$this->id_usu.") where id_usu=".$this->id_usu);
+                            
+                            Flash::success('Su ubicación fue registrada');
+                            Input::delete();
+                            Router::redirect("/");
+                        }
+                        else
+                        {
+                            Flash::info('Error en el ingreso de su ubicacion');
+                            Router::redirect('/');
+                        }
+                    }
+                }
+                else
+                {
+                    Flash::info('Su ubicación ya fue ingresada, si requiere modificarla comuniquese con un administrador');
+                    Router::redirect("/");
+                }
+                
+            }
+            else //NO ES CLIENTE
+            {
+                Flash::info('No posee los privilegios necesarios');
+                Router::redirect("/");
+            } 
+        }
+        else //NO ESTA LOGUEADO
+        {
+            Flash::info('Debe iniciar sesión');
+            Router::redirect("/");
+        }
+    }
 }
