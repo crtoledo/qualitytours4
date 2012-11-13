f2178ad16d400aa6c065543c1290f462af655311
x.katto@gmail.com <x.katto@gmail.com@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-11-13 16:09:49 +0000

    Busqueda de usuario con opciones de busqueda

--- trunk/qualitytours/default/public/ajax/usuario_busqueda.php
+++ trunk/qualitytours/default/public/ajax/usuario_busqueda.php
@@ -2,12 +2,24 @@
 require_once 'connection.php';
 $Db = new db();
 $conn = $Db->open();
-$res = pg_query($conn,"select * from usuario where username_usu ilike '%".$_REQUEST['string']."%'");
+
+$tipo = $_REQUEST['opcion'];
+$usuariobuscar = $_REQUEST['string'];
+
+if ($tipo == "")
+{
+$res = pg_query($conn,"select * from usuario where username_usu ilike '%".$usuariobuscar."%'");
 $arr = pg_fetch_all($res);
+}
+else
+{
+$res = pg_query($conn,"select * from usuario where username_usu ilike '%".$usuariobuscar."%' and rol_usu='".$tipo ."'");
+$arr = pg_fetch_all($res);    
+}
 
 if (is_array($arr) && isset($arr[0]))
 {    
-    echo json_encode($arr); 
+   echo json_encode($arr); 
 }
 else
     return null;
