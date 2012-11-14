06134a9a29984b2532ff677823899d8d3398eca6
x.katto@gmail.com <x.katto@gmail.com@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-11-14 21:12:31 +0000

    Interfaz solicitud por parte del turista

--- qualitytours/default/app/controllers/solicitud_controller.php
+++ qualitytours/default/app/controllers/solicitud_controller.php
@@ -1,46 +1,79 @@
 <?php
+
 load::model('solicitud');
 
-class SolicitudController extends AppController 
-{
-    public function ingresar($id)
-    {
-        if(Auth::is_valid())
-        {
-            
+class SolicitudController extends AppController {
+
+        public function before_filter() {
+        if (!Auth::is_valid()) {
+            Flash::info('Debe iniciar sesión');
+            Router::redirect("/");
+        }
+    }
+    
+    public function ingresar($id) {
+        if (Auth::is_valid()) {
+
             date_default_timezone_set('America/Santiago');
             $nueva_solicitud = new Solicitud();
-            
+
             //se crean los valores
             $nueva_solicitud->id_usu = $id;
             //$nueva_solicitud-> = "";
             $nueva_solicitud->fecha_sol = date("d-m-Y");
             $nueva_solicitud->estado_sol = "pendiente";
             $nueva_solicitud->tipo_sol = "nueva";
-            $nueva_solicitud->observaciones_sol ="No presenta observaciones";
+            $nueva_solicitud->observaciones_sol = "No presenta observaciones";
             $nueva_solicitud->mail_sol = "false";
             $nueva_solicitud->modificaciones_sol = "false";
-            
-            if($nueva_solicitud->save())
+
+            if ($nueva_solicitud->save()) {
+                Router::redirect("/");
+            } else {
+                Flash::info('No se ingreso');
+            }
+        } else {
+            Flash::info('No posee los privilegios necesarios');
+            Router::redirect("/");
+        }
+    }
+
+    public function ver($id) 
+    {
+        $solicitud = new solicitud();
+        //Se verifica que el turista sea el turista que consulta su solicitud
+        if (Auth::get('id') == $id) 
+        {
+            if ($solicitud->find_by_sql("select * from solicitud where id_usu = " . $id . "and estado_sol='pendiente'")) 
+            {
+                $this->fecha = $solicitud->fecha_sol;
+                $this->estado = $solicitud->estado_sol;
+                $this->tipo = $solicitud->tipo_sol;
+                $this->observaciones = $solicitud->observaciones_sol;
+                if ($solicitud->estado_sol== "aprobada")
+                {
+                    $this->colortabla="success";
+                }
+                else if ($solicitud->estado_sol== "pendiente")
+                {
+                    $this->colortabla="warning";
+                }
+                else if ($solicitud->estado_sol== "rechazada")
+                {
+                    $this->colortabla="error";
+                }
+            } 
+            else 
             {
                 Router::redirect("/");
-            }
-            else
-            {
-                Flash::info('No se ingreso');
             }
         }
         else
         {
-            Flash::info('No posee los privilegios necesarios');
             Router::redirect("/");
-        } 
+        }
     }
-       
-    
-    public function ver($id)
-    {
-            
-    }
+
 }
+
 ?>
