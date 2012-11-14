10ca2e026eead88fba4c25adad8699f975868187
x.katto@gmail.com <x.katto@gmail.com@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-11-14 17:41:35 +0000

    Agregacion de campo en el formulario ingresarsolicitud

--- trunk/qualitytours/default/app/models/cliente.php
+++ trunk/qualitytours/default/app/models/cliente.php
@@ -7,6 +7,15 @@ class Cliente extends ActiveRecord
         
     }
     
+    public function before_create()
+    {
+       //valida que solo exista un rut
+      if($this->exists("rut_cli='".$this->rut_cli."'"))
+      { 
+          Flash::error('El rut ingresado ya se encuentra en nuestros registros.'); 
+           return 'cancel'; 
+      }
+    }
 }
 ?>
 
