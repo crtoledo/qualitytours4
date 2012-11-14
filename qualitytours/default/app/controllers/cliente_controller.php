b976b0f5a427cb256069170f41dac8b35495bf1d
x.katto@gmail.com <x.katto@gmail.com@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-11-14 04:47:30 +0000

    Implementacion de solicitudes 60%

--- qualitytours/default/app/controllers/cliente_controller.php
+++ qualitytours/default/app/controllers/cliente_controller.php
@@ -1,170 +1,152 @@
 <?php
+
 load::model('usuario');
 load::model('cliente');
 load::model('servicio');
 load::model('ubicacion');
 load::model('contenido');
 load::model('comentario');
+load::model('solicitud');
 
-class ClienteController extends AppController 
-{
-     
-    
-    public function index()
-    {
+class ClienteController extends AppController {
+
+    public function index() {
         
     }
-    
-    public function before_filter(){
-        
-	}
 
-    public function ingresarsolicitud()
-        { 
-        
-        //verifica si se encuentra logueado
-		if(!Auth::is_valid())
-                {
-                        Flash::info('Debe iniciar sesión');
-			Router::redirect("/");     
-		}
-                else
-                {
-                    if(Input::hasPost('cliente'))
-                    {
-                        $cliente = new Cliente(Input::post('cliente'));
-              
-                        //Obtenemos el id del usuario que queremos convertir a cliente
-                        $id_usuario = $cliente->id;
+    public function before_filter() {
+        if (!Auth::is_valid()) {
+            Flash::info('Debe iniciar sesión');
+            Router::redirect("/");
+        }
+    }
 
-                        //Creamos la instancia
-                        $user= new usuario();
-                
-                        //Obtenemos los datos del usuario mediante su id
-                        $datos_user = $user->find($id_usuario);
-                
-                        //Paso de datos desde usuario encontrado a cliente a ingresar
-                        $id= $datos_user->id;        
-                        $username_usu = $datos_user->username_usu;
-                        $password_usu = $datos_user->password_usu;
-                        $rol_usu = 'turista';
-                        $nombre_usu = $datos_user->nombre_usu;
-                        $apellido_usu = $datos_user->apellido_usu;
-                        $rut_usu = $datos_user->rut_usu;
-                        $email_usu = $datos_user->email_usu;
-                        $estado_usu = "false";
-                        $nombre_cli = $cliente->nombre_cli;
-                        $rut_cli = $cliente->rut_cli;
-                        $giro = $cliente->giro_cli;
-                        $telefono = $cliente->telefono_cli;
-                        $visitas = 0;  
-                        
-                        if($cliente->sql("insert into Cliente (id_usu,username_usu,password_usu,rol_usu,nombre_usu,apellido_usu,rut_usu,email_usu,estado_usu,nombre_cli,rut_cli,giro_cli,telefono_cli,visitas_cli) values(".$id.",'".$username_usu."','".$password_usu."','".$rol_usu."','".$nombre_usu."','".$apellido_usu."','".$rut_usu."','".$email_usu."','".$estado_usu."','".$nombre_cli."','".$rut_cli."','".$giro."','".$telefono."',".$visitas.");"))//&&($user->sql("update Usuario set rol_usu='cliente' where id=".$id)))
-                        {
-                            Flash::success("Solicitud enviada correctamente");
-                            Input::delete();
-                            Router::redirect("solicitud/ingresar/".$id);
-                        }
-                        else
-                        {
-                            Flash::error("Error en el ingreso del cliente");
-                        }
-                        //                Sentencia original
-                        //                $cliente->sql("insert into Cliente (id_usu,username_usu,password_usu,rol_usu,nombre_usu,apellido_usu,rut_usu,email_usu,estado_usu,nombre_cli,rut_cli,giro_cli,telefono_cli,visitas_cli) values(".$id.",'".$username_usu."','".$password_usu."','".$rol_usu."','".$nombre_usu."','".$apellido_usu."','".$rut_usu."','".$email_usu."','".$estado_usu."','".$nombre_cli."','".$rut_cli."','".$giro."','".$telefono."',".$visitas.");");
-                    }
-                } 
-            }    
+    public function ingresarsolicitud() {
+        $id = Auth::get('id');
+        $solicitud = new Solicitud ();
 
-    
-    public function borrar()
-    {
-        if(Input::hasPost('cliente'))
-        {
+        //se verifica si el usuario tiene solicitudes pendientes
+        if (!$solicitud->find("conditions: id_usu=" . Auth::get('id') . " and estado_sol='pendiente'")) {
+            if (Input::hasPost('cliente')) {
+                $cliente = new Cliente(Input::post('cliente'));
+
+                //Obtenemos el id del usuario que queremos convertir a cliente
+                $id_usuario = $cliente->id;
+
+                //Creamos la instancia
+                $user = new usuario();
+
+                //Obtenemos los datos del usuario mediante su id
+                $datos_user = $user->find($id_usuario);
+
+                //Paso de datos desde usuario encontrado a cliente a ingresar
+                $id = $datos_user->id;
+                $username_usu = $datos_user->username_usu;
+                $password_usu = $datos_user->password_usu;
+                $rol_usu = 'turista';
+                $nombre_usu = $datos_user->nombre_usu;
+                $apellido_usu = $datos_user->apellido_usu;
+                $rut_usu = $datos_user->rut_usu;
+                $email_usu = $datos_user->email_usu;
+                $estado_usu = "false";
+                $nombre_cli = $cliente->nombre_cli;
+                $rut_cli = $cliente->rut_cli;
+                $giro = $cliente->giro_cli;
+                $telefono = $cliente->telefono_cli;
+                $visitas = 0;
+
+                if ($cliente->sql("insert into Cliente (id_usu,username_usu,password_usu,rol_usu,nombre_usu,apellido_usu,rut_usu,email_usu,estado_usu,nombre_cli,rut_cli,giro_cli,telefono_cli,visitas_cli) values(" . $id . ",'" . $username_usu . "','" . $password_usu . "','" . $rol_usu . "','" . $nombre_usu . "','" . $apellido_usu . "','" . $rut_usu . "','" . $email_usu . "','" . $estado_usu . "','" . $nombre_cli . "','" . $rut_cli . "','" . $giro . "','" . $telefono . "'," . $visitas . ");")) {//&&($user->sql("update Usuario set rol_usu='cliente' where id=".$id)))
+                    Flash::success("Solicitud enviada correctamente");
+                    Input::delete();
+                    Router::redirect("solicitud/ingresar/" . $id);
+                } else {
+                    Flash::error("Error en el ingreso del cliente");
+                }
+                //                Sentencia original
+                //                $cliente->sql("insert into Cliente (id_usu,username_usu,password_usu,rol_usu,nombre_usu,apellido_usu,rut_usu,email_usu,estado_usu,nombre_cli,rut_cli,giro_cli,telefono_cli,visitas_cli) values(".$id.",'".$username_usu."','".$password_usu."','".$rol_usu."','".$nombre_usu."','".$apellido_usu."','".$rut_usu."','".$email_usu."','".$estado_usu."','".$nombre_cli."','".$rut_cli."','".$giro."','".$telefono."',".$visitas.");");
+            }
+        } else { // en caso de tener solicitudes pendientes se redirecciona a ver la solicitud
+            Router::redirect("solicitud/ver/" . $id);
+        }
+    }
+
+    public function borrar() {
+        if (Input::hasPost('cliente')) {
             
         }
-
     }
-    
-    public function detalle($id,$leng)
-    {
-        
+
+    public function detalle($id, $leng) {
+
         $this->leng = $leng;
-       
-            $this->idiom = "en";
-            $client = new Cliente();
-            $client = $client->find($id);
-            $this->id_cliente = $id;
 
-            //Comprueba que no se actualise el contador de visita si es el mismo dueño del centro turistico
-            if(Session::get("id") == $id)
-            {  
-                 $captura = $client->visitas_cli ;
-                 $actualiza = $captura+0;
-                 $client->sql("update Cliente set visitas_cli=".$actualiza."where id_usu=".$id);
-            }
-            if(Session::get("id")!= $id)
-            {
-                $captura = $client->visitas_cli ;
-                $actualiza = $captura+1;
-                $client->sql("update Cliente set visitas_cli=".$actualiza."where id_usu=".$id);
-            }
-            if(Session::get("id") == null)
-            {
-                $captura = $client->visitas_cli ;
-                $actualiza = $captura+1;
-                $client->sql("update Cliente set visitas_cli=".$actualiza."where id_usu=".$id);
-            }
+        $this->idiom = "en";
+        $client = new Cliente();
+        $client = $client->find($id);
+        $this->id_cliente = $id;
+
+        //Comprueba que no se actualise el contador de visita si es el mismo dueño del centro turistico
+        if (Session::get("id") == $id) {
+            $captura = $client->visitas_cli;
+            $actualiza = $captura + 0;
+            $client->sql("update Cliente set visitas_cli=" . $actualiza . "where id_usu=" . $id);
+        }
+        if (Session::get("id") != $id) {
+            $captura = $client->visitas_cli;
+            $actualiza = $captura + 1;
+            $client->sql("update Cliente set visitas_cli=" . $actualiza . "where id_usu=" . $id);
+        }
+        if (Session::get("id") == null) {
+            $captura = $client->visitas_cli;
+            $actualiza = $captura + 1;
+            $client->sql("update Cliente set visitas_cli=" . $actualiza . "where id_usu=" . $id);
+        }
 
 
-            $this->nombre_cliente = $client->nombre_cli;
-            $this->mostrar = $client->visitas_cli+1;
-            $contenido = new Contenido();
-            $contenido = $contenido->find("conditions: id_usu=".$id);
-            $this->contenido = $contenido;
+        $this->nombre_cliente = $client->nombre_cli;
+        $this->mostrar = $client->visitas_cli + 1;
+        $contenido = new Contenido();
+        $contenido = $contenido->find("conditions: id_usu=" . $id);
+        $this->contenido = $contenido;
 
 
-            $services = new Servicio();
-            $services = $services->find_all_by('id_usu', $id);
-            $this->array_servicios = $services;
+        $services = new Servicio();
+        $services = $services->find_all_by('id_usu', $id);
+        $this->array_servicios = $services;
 
-            //2- Necesario para cargar el mapa
-            $ubicacion = new Ubicacion();
-            $ubicacion = $ubicacion->find_all_by('id_usu', $id);
-            if($ubicacion != null)
-            {
-                $this->latitud = $ubicacion[0]->latitud_ubi; //El [0] debido a que nos entrega un array
-                $this->longitud = $ubicacion[0]->longitud_ubi;
-            }
+        //2- Necesario para cargar el mapa
+        $ubicacion = new Ubicacion();
+        $ubicacion = $ubicacion->find_all_by('id_usu', $id);
+        if ($ubicacion != null) {
+            $this->latitud = $ubicacion[0]->latitud_ubi; //El [0] debido a que nos entrega un array
+            $this->longitud = $ubicacion[0]->longitud_ubi;
+        }
 
-                $comentario = new Comentario();
-                //validar si existen comentarios
-                if($comentario->count("conditions: estado_com='t' and cli_id_usu=".$id) == 0)
-                {
-                  $this->cont = 0;
-                }
-                if($comentario->count("conditions: estado_com='t' and cli_id_usu=".$id) > 0)
-                {
-                  $this->numComentarios = $comentario->count("conditions: estado_com='t' and cli_id_usu=".$id);  
-                  $this->cont = 1;
-                }
-                $arr= $comentario->find("conditions: estado_com='t' and cli_id_usu=".$id,"order: id ASC");
-                $contador = 0;
-                $user = new Usuario();
-                foreach ($arr as $comentario )
-                {
+        $comentario = new Comentario();
+        //validar si existen comentarios
+        if ($comentario->count("conditions: estado_com='t' and cli_id_usu=" . $id) == 0) {
+            $this->cont = 0;
+        }
+        if ($comentario->count("conditions: estado_com='t' and cli_id_usu=" . $id) > 0) {
+            $this->numComentarios = $comentario->count("conditions: estado_com='t' and cli_id_usu=" . $id);
+            $this->cont = 1;
+        }
+        $arr = $comentario->find("conditions: estado_com='t' and cli_id_usu=" . $id, "order: id ASC");
+        $contador = 0;
+        $user = new Usuario();
+        foreach ($arr as $comentario) {
 
-                   $this->detalle[$contador] = $arr[$contador]->detalle_com;
-                   $this->fecha[$contador] = $arr[$contador]->fecha_com;  
-                   $nombre_usuario[$contador] = $user->find($arr[$contador]->id_usu);
-                   $this->nombre[$contador] = $nombre_usuario[$contador]->nombre_usu;
-                   $this->id[$contador] = $arr[$contador]->id_usu;
-                   $this->idComentario[$contador] = $arr[$contador]->id;
+            $this->detalle[$contador] = $arr[$contador]->detalle_com;
+            $this->fecha[$contador] = $arr[$contador]->fecha_com;
+            $nombre_usuario[$contador] = $user->find($arr[$contador]->id_usu);
+            $this->nombre[$contador] = $nombre_usuario[$contador]->nombre_usu;
+            $this->id[$contador] = $arr[$contador]->id_usu;
+            $this->idComentario[$contador] = $arr[$contador]->id;
 
-                   $contador++;
-                }
-                $this->contador= $contador;
-      
+            $contador++;
+        }
+        $this->contador = $contador;
     }
-     
+
 }
 
