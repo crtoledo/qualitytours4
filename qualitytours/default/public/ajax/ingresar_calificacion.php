c98e10d0a72ec008c9968b4676a38c987667b966
kerosene90 <kerosene90@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-11-14 21:24:41 +0000

    Pasando variables al ajax de calificación (id_cli y id_usu)

--- qualitytours/default/public/ajax/ingresar_calificacion.php
+++ qualitytours/default/public/ajax/ingresar_calificacion.php
@@ -7,7 +7,9 @@ $votacion = $_POST['rating'];
 
 $Db->close($conn);
 
- echo $votacion; 
+ echo "Calificación: ".$votacion; 
+ echo "<br>ID cliente: ".$_POST['id_cli'];
+ echo "<br>ID usuario q califica: ".$_POST['id_usu'];
  
  
 ?>
