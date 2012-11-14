41e57319679a1a78715e3c7c3011e402e0ecf391
gbriones1234@gmail.com <gbriones1234@gmail.com@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-11-14 20:36:55 +0000

    calificacion ...

--- qualitytours/default/public/ajax/ingresar_calificacion.php
+++ qualitytours/default/public/ajax/ingresar_calificacion.php
@@ -2,26 +2,12 @@
 require_once 'connection.php';
 $Db = new db();
 $conn = $Db->open();
-setlocale ( LC_TIME, 'spanish' );
-$fecha = strftime(date('d/m/Y'));
-$cal = $_REQUEST['cal'];
-$id_usu= $_REQUEST['id_usu'];
-$id_cli = $_REQUEST['id_cli'];
 
+$votacion = $_POST['rating'];
 
-
-
-$res = pg_query($conn,"INSERT INTO calificacion(id_usu, cli_id_usu, valor_cal, fecha_cal) VALUES (".$id_usu.",".$id_cli.",".$cal.",'".$fecha."')");
-
-if($res)
-{
-    echo 'exito';
-}
-else
-{
-    echo 'error';
-}
 $Db->close($conn);
 
-
+ echo $votacion; 
+ 
+ 
 ?>
