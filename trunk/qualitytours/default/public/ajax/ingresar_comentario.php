32cd81b8d8495d4a95cf1f43842f9e37fe352d0e
gbriones1234@gmail.com <gbriones1234@gmail.com@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-11-15 04:53:14 +0000

    implementando comentario via ajax 10%

--- trunk/qualitytours/default/public/ajax/ingresar_comentario.php
+++ trunk/qualitytours/default/public/ajax/ingresar_comentario.php
@@ -1,5 +1,17 @@
 <?php
-if($_POST['mensaje'] != ''){
-	echo $_POST['mensaje'];
-}
+ require_once 'connection.php';
+    $Db = new db();
+    $conn = $Db->open();
+    setlocale ( LC_TIME, 'spanish' );
+    $fecha = strftime(date('Y/m/d'));
+    $id_usu = $_POST['id_usu'];
+    $detalle= $_POST['mensaje'];
+    $id_cli = $_POST['id_cli'];
+   
+    
+
+$res=  pg_query("INSERT INTO comentario(id_usu, cli_id_usu, fecha_com, detalle_com, estado_com)
+    VALUES (".$id_usu.",".$id_cli.",'".$fecha."','".$detalle."','t')");
+
+
 ?>
