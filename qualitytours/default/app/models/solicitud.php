59d2027a4b563c9cf2ce1fb5e76a0fdfb162f800
x.katto@gmail.com <x.katto@gmail.com@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-11-15 22:48:33 +0000

    Solicitud por parte del turista completa.. falta la traduccion

--- qualitytours/default/app/models/solicitud.php
+++ qualitytours/default/app/models/solicitud.php
@@ -1,6 +1,9 @@
 <?php
 class Solicitud extends ActiveRecord
 {
-
+    public function getsolicitud($page,$datoabuscar, $ppage=10)
+    {
+       return $this->paginate("id_usu=".$datoabuscar ,"page: $page", "per_page: $ppage");
+    }
 }
 ?>
