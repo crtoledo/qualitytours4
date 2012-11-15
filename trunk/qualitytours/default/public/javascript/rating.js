c07551b323aec4c47a7718277543dd76d25550b5
gbriones1234@gmail.com <gbriones1234@gmail.com@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-11-15 00:08:37 +0000

    calificacion 99,9%...

--- trunk/qualitytours/default/public/javascript/rating.js
+++ trunk/qualitytours/default/public/javascript/rating.js
@@ -94,6 +94,7 @@ jQuery.fn.rating = function(url, options) {
                                     id: container.id,
                                     id_cli: id_cli,
                                     id_usu: id_usu
+                                  
 				},
 				function(data){
     				//$(".starRpta").html(data);
