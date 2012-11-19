0bbced2336a6aef227a8b032997c0fd1144e0b94
kerosene90 <kerosene90@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-11-19 14:25:24 +0000

    arreglo busqueda usuarios sin criterio seleccionado

--- qualitytours/default/public/ajax/usuario_busqueda.php
+++ qualitytours/default/public/ajax/usuario_busqueda.php
@@ -1,25 +1,26 @@
 <?php
+
 require_once 'connection.php';
 $Db = new db();
 $conn = $Db->open();
 
-$tipo = $_REQUEST['opcion'];
+if (isset($_REQUEST['opcion']))
+    $tipo = $_REQUEST['opcion'];
+else
+    $tipo='no_seleccionado';
+
 $usuariobuscar = $_REQUEST['string'];
 
-if ($tipo == "")
-{
-$res = pg_query($conn,"select * from usuario where username_usu ilike '%".$usuariobuscar."%'");
-$arr = pg_fetch_all($res);
-}
-else
-{
-$res = pg_query($conn,"select * from usuario where username_usu ilike '%".$usuariobuscar."%' and rol_usu='".$tipo ."'");
-$arr = pg_fetch_all($res);    
+if ($tipo == "no_seleccionado") {
+    $res = pg_query($conn, "select * from usuario where username_usu ilike '%" . $usuariobuscar . "%'");
+    $arr = pg_fetch_all($res);
+} else {
+    $res = pg_query($conn, "select * from usuario where username_usu ilike '%" . $usuariobuscar . "%' and rol_usu='" . $tipo . "'");
+    $arr = pg_fetch_all($res);
 }
 
-if (is_array($arr) && isset($arr[0]))
-{    
-   echo json_encode($arr); 
+if (is_array($arr) && isset($arr[0])) {
+    echo json_encode($arr);
 }
 else
     return null;
