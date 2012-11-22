2690e5ef0295edf8f2ec72d23074a64a4eea51a6
gbriones1234@gmail.com <gbriones1234@gmail.com@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-11-22 18:58:30 +0000

    la calificacion se actualiza si se realizó anteriormente ...

--- trunk/qualitytours/default/public/ajax/ingresar_calificacion.php
+++ trunk/qualitytours/default/public/ajax/ingresar_calificacion.php
@@ -10,32 +10,80 @@
  
     $id_usu = $_POST['id_usu'];
     $id_cli = $_POST['id_cli'];
- 
+   
+     
  if($id_usu != $id_cli)
  {
         $validar = pg_num_rows(pg_query("select * from calificacion where id_usu=" . $id_usu));
-
     if ($validar == 0) {
         $res = pg_query("INSERT INTO calificacion(id_usu, cli_id_usu, valor_cal, fecha_cal)
         VALUES (" . $_POST['id_usu'] . "," . $_POST['id_cli'] . "," . $votacion . ",'" . $fecha . "')");
+       
+        
+        if($votacion == "1")
+        {
+            echo "<div class='alert alert-success'>  <b>1</b> </div>";
+        }
+        if($votacion == "2")
+        {
+            echo "<div class='alert alert-success'> <b>2</b> </div>";
+        }
+        if($votacion == "3")
+        {
+            echo "<div class='alert alert-success'>  <b>3</b> </div>";
+        }
+        if($votacion == "4")
+        {
+            echo "<div class='alert alert-success'>  <b>4</b> </div>";
+        }
+        if($votacion == "5")
+        {
+            echo "<div class='alert alert-success'> <b>5</b> </div>";
+        }
+        
     }
     else
     {
+        
+        if($votacion == "1")
+        {
+            echo "<div class='alert alert-success'>  <b>1</b> </div>";
+        }
+        if($votacion == "2")
+        {
+            echo "<div class='alert alert-success'> <b>2</b> </div>";
+        }
+        if($votacion == "3")
+        {
+            echo "<div class='alert alert-success'>  <b>3</b> </div>";
+        }
+        if($votacion == "4")
+        {
+            echo "<div class='alert alert-success'>  <b>4</b> </div>";
+        }
+        if($votacion == "5")
+        {
+            echo "<div class='alert alert-success'> <b>5</b> </div>";
+        }
         ?>
-            <script>
-               alert("Lo sentimos no puede realizar una calificacion");
-           </script>
+           
+           <?php  $res2 = pg_query("UPDATE calificacion  set valor_cal=".$votacion." WHERE id_usu=". $_POST['id_usu']."and cli_id_usu=". $_POST['id_cli']); ?>
+            
+            <?php 
+              if(!$res2)
+              {
+                   echo "<div class='alert alert-success'> <b>Debe iniciar sesión</b> </div>";
+              }
+            ?>
     <?php
 
     }
      
  }
- else
- {?>
-     <script>
-        alert("Lo sentimos no puede realizar una calificacion");
-      </script>
- <?php }
+
+
+     
+ 
  
     $Db->close($conn);
 ?>
