2c8707f1371290b3ed1db479f2f1d619ebcef964
x.katto@gmail.com <x.katto@gmail.com@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-11-23 14:09:56 +0000

    Validaciones a solicitud administrador

--- trunk/qualitytours/default/app/controllers/solicitud_controller.php
+++ trunk/qualitytours/default/app/controllers/solicitud_controller.php
@@ -18,8 +18,8 @@ class SolicitudController extends AppController {
 //**********    Funciones para el usuario    **********//
 //****************************************************//    
 ///////////////////////////////////////////////////////      
-    
-    
+
+
     public function ingresar($id) {
         // se comprueba que sea turista el que ingresa la solicitud
         if (Auth::get('rol_usu') != "cliente") {
@@ -133,14 +133,14 @@ class SolicitudController extends AppController {
 //**********  Fin funciones para el usuario  **********//
 //****************************************************//    
 /////////////////////////////////////////////////////// 
-    
-    
+
+
 ///////////////////////////////////////////////////////////
 //******************************************************//    
 //********** Funciones para el administrador **********//
 //****************************************************//    
 ///////////////////////////////////////////////////////  
-    
+
     Public function buscar() {
         
     }
@@ -149,30 +149,37 @@ class SolicitudController extends AppController {
         if (Auth::get("rol_usu") == "administrador") {
 
             $datos_solicitud = new solicitud();
-            // se valida que al solicitud corresponda al usuario
-            if ($datos_solicitud->find_by_sql("select * from solicitud where id=" . $solicitud . " and id_usu = " . $usuario)) {
-                // se obtienen los datos del usuario que quiere ser cliente
-                $datos_cliente = New Cliente;
-                $datos_cliente = $datos_cliente->find($usuario);
 
-                $this->id_usuario = $usuario;
-                $this->id_solicitud = $solicitud;
-                $this->nombre_cli = $datos_cliente->nombre_cli;
-                $this->nombre_usu = $datos_cliente->nombre_usu;
-                $this->rut_usu = $datos_cliente->rut_usu;
-                $this->rut_cli = $datos_cliente->rut_cli;
-                $this->nombre_usu = $datos_cliente->nombre_usu;
-                $this->giro_cli = $datos_cliente->giro_cli;
-                $this->telefono_cli = $datos_cliente->telefono_cli;
-                $this->tipo_plan = $datos_cliente->tipo_plan;
-                
-                //datos de la solicitud para mostrar en la vista
-                $this->estado_sol = $datos_solicitud->estado_sol;
-                $this->observaciones = $datos_solicitud->observaciones_sol;
-                $this->fecha_sol = $datos_solicitud->fecha_sol;
+            // valido que los datos obtenidos sean numericos
+            if (is_numeric($solicitud) && is_numeric($usuario)) {
+                // se valida que al solicitud corresponda al usuario
+                if ($datos_solicitud->find_by_sql("select * from solicitud where id=" . $solicitud . " and id_usu = " . $usuario)) {
+                    // se obtienen los datos del usuario que quiere ser cliente
+                    $datos_cliente = New Cliente;
+                    $datos_cliente = $datos_cliente->find($usuario);
 
-                // se cargan los datos de la solicitud... para observaciones
-                $this->solicitud = $datos_solicitud;
+                    $this->id_usuario = $usuario;
+                    $this->id_solicitud = $solicitud;
+                    $this->nombre_cli = $datos_cliente->nombre_cli;
+                    $this->nombre_usu = $datos_cliente->nombre_usu;
+                    $this->rut_usu = $datos_cliente->rut_usu;
+                    $this->rut_cli = $datos_cliente->rut_cli;
+                    $this->nombre_usu = $datos_cliente->nombre_usu;
+                    $this->giro_cli = $datos_cliente->giro_cli;
+                    $this->telefono_cli = $datos_cliente->telefono_cli;
+                    $this->tipo_plan = $datos_cliente->tipo_plan;
+
+                    //datos de la solicitud para mostrar en la vista
+                    $this->estado_sol = $datos_solicitud->estado_sol;
+                    $this->observaciones = $datos_solicitud->observaciones_sol;
+                    $this->fecha_sol = $datos_solicitud->fecha_sol;
+
+                    // se cargan los datos de la solicitud... para observaciones
+                    $this->solicitud = $datos_solicitud;
+                } else {
+                    Flash::info('Datos no corresponden a la solicitud');
+                    Router::redirect("/solicitud/buscar");
+                }
             } else {
                 Flash::info('Datos no corresponden a la solicitud');
                 Router::redirect("/solicitud/buscar");
@@ -207,72 +214,74 @@ class SolicitudController extends AppController {
 
     Public function aceptarsolicitud($id_cliente, $id_solicitud) {
         if (Auth::get("rol_usu") == "administrador") {
-            
-            $comprobar_solicitud = new solicitud();
-            // se valida que al solicitud corresponda al usuario
-            if ($comprobar_solicitud->find_by_sql("select * from solicitud where id=" . $id_solicitud . " and id_usu = " . $id_cliente)) {
-                date_default_timezone_set('America/Santiago');
+            // valido que los datos obtenidos sean numericos
+            if (is_numeric($id_cliente) && ($id_solicitud)) {
+                $comprobar_solicitud = new solicitud();
+                // se valida que al solicitud corresponda al usuario
+                if ($comprobar_solicitud->find_by_sql("select * from solicitud where id=" . $id_solicitud . " and id_usu = " . $id_cliente)) {
+                    date_default_timezone_set('America/Santiago');
 
-                $datos_actualizacion_cliente = new cliente;
-                $datos_actualizacion_usuario = new usuario;
-                $datos_actualizacion_solicitud = new solicitud;
+                    $datos_actualizacion_cliente = new cliente;
+                    $datos_actualizacion_usuario = new usuario;
+                    $datos_actualizacion_solicitud = new solicitud;
 
-                $fecha_inicio = date("d-m-Y");
-                $fecha_fin = date('d-m-Y', strtotime('+1 Year'));
+                    $fecha_inicio = date("d-m-Y");
+                    $fecha_fin = date('d-m-Y', strtotime('+1 Year'));
 
-                $sentencia = "UPDATE cliente SET id_sol=" . $id_solicitud . ", rol_usu='cliente', estado_usu= true, fecha_ini_sus='" . $fecha_inicio . "', fecha_fin_sus='" . $fecha_fin . "' WHERE id_usu=" . $id_cliente;
-                $sentencia_actualizar_rol = "UPDATE usuario SET id_sol=" . $id_solicitud . ", rol_usu='cliente' WHERE id=" . $id_cliente;
-                $sentencia_actualizar_solicitud = "UPDATE solicitud SET estado_sol='Aceptada' WHERE id=" . $id_solicitud;
+                    $sentencia = "UPDATE cliente SET id_sol=" . $id_solicitud . ", rol_usu='cliente', estado_usu= true, fecha_ini_sus='" . $fecha_inicio . "', fecha_fin_sus='" . $fecha_fin . "' WHERE id_usu=" . $id_cliente;
+                    $sentencia_actualizar_rol = "UPDATE usuario SET id_sol=" . $id_solicitud . ", rol_usu='cliente' WHERE id=" . $id_cliente;
+                    $sentencia_actualizar_solicitud = "UPDATE solicitud SET estado_sol='Aceptada' WHERE id=" . $id_solicitud;
 
-                if ($datos_actualizacion_cliente->sql($sentencia) && $datos_actualizacion_usuario->sql($sentencia_actualizar_rol) && $datos_actualizacion_solicitud->sql($sentencia_actualizar_solicitud)) {
-                    Flash::info('Solicitud aceptada, cliente ingresado');
-                    Router::redirect("/");
+                    if ($datos_actualizacion_cliente->sql($sentencia) && $datos_actualizacion_usuario->sql($sentencia_actualizar_rol) && $datos_actualizacion_solicitud->sql($sentencia_actualizar_solicitud)) {
+                        Flash::info('Solicitud aceptada, cliente ingresado');
+                        Router::redirect("/solicitud/administrar/".$id_solicitud."/".$id_cliente);
+                    } else {
+                        
+                    }
                 } else {
-                    
+                    Flash::info('Datos no corresponden a la solicitud');
+                    Router::redirect("/");
                 }
-            }
-            else
-            {
+            } else {
                 Flash::info('Datos no corresponden a la solicitud');
-            Router::redirect("/");
+                Router::redirect("/");
             }
         } else {
             Flash::info('No tiene los privilegios necesarios');
             Router::redirect("/");
         }
     }
-    
-    Public function rechazar ($id,$usuario)
-    {
+
+    Public function rechazar($id, $usuario) {
         if (Auth::get("rol_usu") == "administrador") {
-            
-            $solicitud_rechazar = new solicitud ();
-            if($solicitud_rechazar->find($id)){
-                $solicitud_rechazar->activo_sol = "false";
-                $solicitud_rechazar->estado_sol = "Rechazada";
-                
-                if ($solicitud_rechazar->update()){
-                    Flash::info("Solicitud Rechazada");
-                    Router::redirect("/solicitud/administrar/".$id."/".$usuario);
+            // valido que los datos obtenidos sean numericos
+            if (is_numeric($id) && is_numeric($usuario)) {
+                $solicitud_rechazar = new solicitud ();
+                if ($solicitud_rechazar->find($id)) {
+                    $solicitud_rechazar->activo_sol = "false";
+                    $solicitud_rechazar->estado_sol = "Rechazada";
+
+                    if ($solicitud_rechazar->update()) {
+                        Flash::info("Solicitud Rechazada");
+                        Router::redirect("/solicitud/administrar/" . $id . "/" . $usuario);
+                    } else {
+                        Flash::error("Error al actualizar");
+                    }
+                } else {
+                    Flash::error("Error al rechazar la solicitud");
                 }
-                else
-                {
-                    Flash::error("Error al actualizar");
-                }
+            } else {
+                Flash::info('Datos no corresponden a la solicitud');
+                Router::redirect("/");
             }
-            else
-            {
-                Flash::error("Error al rechazar la solicitud");
-            }
-            
-        }else {
+        } else {
             Flash::info('No tiene los privilegios necesarios');
             Router::redirect("/");
         }
-        
     }
 
 }
+
 ///////////////////////////////////////////////////////////
 //******************************************************//    
 //******** Fin funciones para el administrador ********//
