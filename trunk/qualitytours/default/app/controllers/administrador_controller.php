f88f7f6b1ff964bae9eff9394486342e4a339a66
x.katto@gmail.com <x.katto@gmail.com@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-11-23 02:36:57 +0000

    Solicitud lista en 80%... modificacion de interfaz para usuario y validaciones nuevas

--- trunk/qualitytours/default/app/controllers/administrador_controller.php
+++ trunk/qualitytours/default/app/controllers/administrador_controller.php
@@ -1,50 +1,45 @@
 <?php
+
 load::model('administrador');
 load::model('usuario');
 
 class AdministradorController extends AppController {
-     
-    
-    public function index(){
-   
-    }
-   public function before_filter(){
-        //verifica si se encuentra logueado
-		if(!Auth::is_valid())
-                {
-                        Flash::info('Debe iniciar sesión');
-			Router::redirect("/");
-		}
-            //verifica si el rol pertenece como corresponde
-            else
-            {
-                if(Auth::get('rol_usu') != 'administrador')
-                {
-                    Flash::info('No posee los privilegios necesarios');
-		    Router::redirect("/");
-                }
-            }            
-	}
 
-    
-    public function ingresar($id,$leng)
-    {
-        $this->leng= $leng;
+    public function index() {
         
+    }
+
+    public function before_filter() {
+        //verifica si se encuentra logueado
+        if (!Auth::is_valid()) {
+            Flash::info('Debe iniciar sesión');
+            Router::redirect("/");
+        }
+        //verifica si el rol pertenece como corresponde
+        else {
+            if (Auth::get('rol_usu') != 'administrador') {
+                Flash::info('No posee los privilegios necesarios');
+                Router::redirect("/");
+            }
+        }
+    }
+
+    public function ingresar($id, $leng) {
+        $this->leng = $leng;
+
         $administrador = new Administrador();
- 
+
         //Obtenemos el id del usuario que queremos convertir a administrador
         $id_usuario = $id;
 
         //Creamos la instancia
-        $user= new usuario();
-                
+        $user = new usuario();
+
         //Obtenemos los datos del usuario mediante su id
         $datos_user = $user->find($id_usuario);
-        if($datos_user->rol_usu != "administrador")
-        {
+        if ($datos_user->rol_usu != "administrador" && $datos_user->rol_usu != "cliente") {
             //Paso de datos desde usuario encontrado a administrador a ingresar
-            $id= $datos_user->id;        
+            $id = $datos_user->id;
             $username_usu = $datos_user->username_usu;
             $password_usu = $datos_user->password_usu;
             $rol_usu = "administrador";
@@ -54,31 +49,17 @@ class AdministradorController extends AppController {
             $email_usu = $datos_user->email_usu;
             $estado_usu = $datos_user->estado_usu;
 
-            if(($administrador->sql("insert into Administrador  values(".$id.",0,'".$username_usu."','".$password_usu."','".$rol_usu."','".$nombre_usu."','".$apellido_usu."','".$email_usu."','".$estado_usu."');"))&& ( $user->sql("update Usuario set rol_usu='administrador' where id=".$id)))
-            {   
-                Flash::success($username_usu." ahora es administrador");
-                Router::redirect("usuario/buscar/".$leng);
-            }
-            else
-            {
+            if (($administrador->sql("insert into Administrador  values(" . $id . ",0,'" . $username_usu . "','" . $password_usu . "','" . $rol_usu . "','" . $nombre_usu . "','" . $apellido_usu . "','" . $email_usu . "','" . $estado_usu . "');")) && ( $user->sql("update Usuario set rol_usu='administrador' where id=" . $id))) {
+                Flash::success($username_usu . " ahora es administrador");
+                Router::redirect("usuario/buscar/" . $leng);
+            } else {
                 Flash::error("Error en el ingreso del administrador");
-                Router::redirect("usuario/buscar/".$leng);
+                Router::redirect("usuario/buscar/" . $leng);
             }
+        } else {
+            Flash::error($datos_user->username_usu . " no se puede convertir en administrador");
+            Router::redirect("usuario/buscar/" . $leng);
         }
-        else
-        {
-            Flash::error($datos_user->username_usu." ya es administrador");
-            Router::redirect("usuario/buscar/".$leng);
-        }
-            
-      } 
-    
-        
-        
-    
-        
-        
-        
-   
+    }
 
 }
