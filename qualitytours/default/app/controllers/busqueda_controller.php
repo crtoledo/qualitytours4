bbb2cc87c33ff77b75978b40ab6f5880e6d2e674
kerosene90 <kerosene90@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-11-27 22:45:58 +0000

    Ingreso Busqueda (TESTEAR XFAA) notas sobre el funcionamiento: solo se ingresa la busqueda si el usuario esta logueado y si los resultados encontrados son mayor a 1 cuando no se encuentra una busqueda, se hace insert si se encuentra q la busqueda ya se hizo, se suma 1 al contador y se guardan los resultados encontrados de la ultima vez q se realizó la busqueda

--- qualitytours/default/app/controllers/busqueda_controller.php
+++ qualitytours/default/app/controllers/busqueda_controller.php
@@ -1,9 +1,26 @@
 <?php
 load::model('ubicacion');
+load::model('usuario');
 
 class BusquedaController extends AppController 
 {
-     
+    
+    public function before_filter() 
+    {
+        //Obtenemos el usuario si es que esta logueado
+        if (Auth::is_valid())
+        {
+            $usuario = new Usuario();
+            $usuario->find(Auth::get('id'));
+            $this->username_usu = $usuario->username_usu;
+            $this->id_usu = $usuario->id;
+        }
+        else
+        {
+            $this->username_usu = "NOREGISTRADO";
+            $this->id_usu = 0;
+        }
+    }
     
     public function index()
     {
@@ -16,6 +33,8 @@ class BusquedaController extends AppController
     }
     public function buscando()
     {
+        
+        
         if (Input::hasPost('string'))
         {
             $this->string = Input::post('string');
