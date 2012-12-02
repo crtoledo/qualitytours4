36de5135c7ee678d23d20445090d8a87c8af0111
x.katto@gmail.com <x.katto@gmail.com@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-12-02 03:14:14 +0000

    Notificaciones para administrador lista.. falta traducir

--- trunk/qualitytours/default/public/ajax/notificaciones.php
+++ trunk/qualitytours/default/public/ajax/notificaciones.php
@@ -3,7 +3,9 @@ require_once 'connection.php';
 $Db = new db();
 $conn = $Db->open();
 
-$resu = pg_query($conn,"select count(*) as datos from solicitud where mail_sol = true union all select count(*) from solicitud where modificaciones_sol= true union all select count(*) from solicitud where estado_sol='Pendiente'");
+$resu = pg_query($conn,"select count(*) as datos from solicitud where mail_sol = true and estado_sol='Esperando' and activo_sol = true
+union all select count(*) from solicitud where modificaciones_sol= true and estado_sol='Pendiente' and activo_sol= true
+union all select count(*) from solicitud where estado_sol='Pendiente' and activo_sol= true");
 $res = pg_fetch_all($resu);
 
 if (is_array($res) && isset($res[0]))
---
+++ trunk/qualitytours/default/public/ajax/notificacionesdetalles.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * 1: Solicitud que esta pendiente de revision
+ * 2: Indica que el cliente ya mando el mail
+ * 3: La solicitud fue modificada
+ */
+
+require_once 'connection.php';
+$Db = new db();
+$conn = $Db->open();
+
+$tipo = $_REQUEST['opcion'];
+
+if ($tipo == "1") {
+    $resu = pg_query($conn, "select solicitud.id, cliente.id_usu, cliente.username_usu, solicitud.fecha_sol, solicitud.estado_sol, solicitud.tipo_sol 
+from solicitud join cliente on solicitud.id_usu= cliente.id_usu
+where activo_sol= true and estado_sol ='Pendiente'");
+    $res = pg_fetch_all($resu);
+} else if ($tipo == "2") {
+    $resu = pg_query($conn, "select solicitud.id, cliente.id_usu, cliente.username_usu, solicitud.fecha_sol, solicitud.estado_sol, solicitud.tipo_sol  
+from solicitud join cliente on solicitud.id_usu= cliente.id_usu
+where mail_sol= true and activo_sol= true and estado_sol ='Esperando'");
+    $res = pg_fetch_all($resu);
+} else if ($tipo == "3") {
+    $resu = pg_query($conn, "select solicitud.id, cliente.id_usu, cliente.username_usu, solicitud.fecha_sol, solicitud.estado_sol, solicitud.tipo_sol 
+from solicitud join cliente on solicitud.id_usu= cliente.id_usu
+where modificaciones_sol=true and activo_sol= true and estado_sol ='Pendiente'");
+    $res = pg_fetch_all($resu);
+}
+
+if (is_array($res) && isset($res[0])) {
+    echo json_encode($res);
+}
+else
+    return null;
+$Db->close($conn);
+?>
