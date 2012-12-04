97e3516a4173a761fe088070554b135936cc6e50
kerosene90 <kerosene90@10869b67-4402-b16d-0fc9-9726bbe9c4f6>
2012-12-04 15:50:52 +0000

    ajax busqeuda criterio 4 listo

--- qualitytours/default/public/ajax/busqueda_gral_c4.php
+++ qualitytours/default/public/ajax/busqueda_gral_c4.php
@@ -1,9 +1,36 @@
 <?php
 
-//BUSQUEDA PARA EL MENU NAV BAR (BUSQUEDA GENERAL)CRITERIO 4 - categorias espaniol
+//BUSQUEDA PARA EL MENU NAV BAR (BUSQUEDA GENERAL)CRITERIO 4 - busqueda categorias (espaniol)
 require_once 'connection.php';
 $Db = new db();
 $conn = $Db->open();
 
+//centros turisticos que contengan el string en la region
+$string =
+"select count (categoria.nombre_cat) as categorias_encontradas, categoria.id_usu as id_cliente, ubicacion.region_ubi as region, ubicacion.ciudad_ubi as ciudad, cliente.nombre_cli as nombre_cliente, cliente.visitas_cli as visitas, coalesce(AVG(calificacion.valor_cal),0) as promedio
+from categoria
+join ubicacion on categoria.id_usu = ubicacion.id_usu
+join cliente on categoria.id_usu = cliente.id_usu
+full join calificacion on categoria.id_usu = calificacion.cli_id_usu
+where categoria.nombre_cat ilike '%".$_REQUEST['string']."%' 
+group by id_cliente, region, ciudad, nombre_cliente, visitas
+order by categorias_encontradas desc, promedio desc, visitas desc
+limit 20;";
 
+$resu = pg_query($conn, $string);
+$res = pg_fetch_all($resu);
+
+if (is_array($res) && isset($res[0])) 
+{
+    echo json_encode($res);
+} 
+else 
+{
+    return null;
+}
+
+$Db->close($conn);
 ?>
+
+
+
